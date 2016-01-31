b7bc54bfb6c46e665c31a06190bb506f0fe13bd6
Paulo Diniz <paulo.rc.diniz@gmail.com>
2016-01-31 19:07:33 +0100

    starting exercises

---
+++ elixir/bob/bob.exs
@@ -0,0 +1,22 @@
+defmodule Bob do
+  def hey(input) do
+    cond do
+        empty?(input) -> "Fine. Be that way!"
+        question?(input) -> "Sure."
+        shouting?(input) -> "Whoa, chill out!"
+        true ->  "Whatever."
+    end
+  end
+  
+  defp shouting?(input) do
+    String.upcase(input) == input 
+  end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp empty?(input) do
+    String.strip(input) == ""
+  end
+end
